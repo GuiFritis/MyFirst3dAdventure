f49e8a363446c8d1dc7754c4e71bd5fd983cbe8e
GuiFritis <Guilhermecfritis@gmail.com>
2022-07-18 09:47:45 -0300

    add uiFeedback to guns

--- GuilhermeFritis-FinalGame/Assets/Scripts/Gun/GunShootLimit.cs
+++ GuilhermeFritis-FinalGame/Assets/Scripts/Gun/GunShootLimit.cs
@@ -1,13 +1,20 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimit : GunBase
-{
+{    
+    public List<UIGunUpdater> uIGunUpdaters;
     public int maxShoot = 5;
     public float cooldown = 1f;
     
     private int _curShots = 0;
+
+    void Awake()
+    {
+        GetAllUIs();
+    }
 
     protected override IEnumerator ShootCoroutine()
     {
@@ -16,6 +23,7 @@ public class GunShootLimit : GunBase
             Shoot();
             _curShots++;
             CheckAmmo();
+            UpdateUI();
             yield return new WaitForSeconds(fireRate);
         } 
     }
@@ -39,8 +47,25 @@ public class GunShootLimit : GunBase
         while(time < cooldown)
         {
             time += Time.deltaTime;
+            foreach (var item in uIGunUpdaters)
+            {
+                item.UpdateValue(time/cooldown);
+            }
             yield return new WaitForEndOfFrame();
         }
         _curShots = 0;
+    }
+
+    private void UpdateUI()
+    {
+        foreach (var item in uIGunUpdaters)
+        {
+            item.UpdateValue(maxShoot, _curShots);
+        }
+    }
+
+    private void GetAllUIs()
+    {
+        uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
     }
 }
---
+++ GuilhermeFritis-FinalGame/Assets/Scripts/Gun/UIGunUpdater.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIGunUpdater : MonoBehaviour
+{
+    public Image uiImage;
+    public float duration = .1f;
+    public Ease ease = Ease.InBack;
+    
+    private Tween _curTween;
+
+    private void OnValidate()
+    {
+        if(uiImage == null)
+        {
+            uiImage = gameObject.GetComponent<Image>();
+        }
+    }
+
+    public void UpdateValue(float val)
+    {
+        uiImage.fillAmount = val;
+    }
+
+    public void UpdateValue(float max, float cur)
+    {
+        if(_curTween != null)
+        {
+            _curTween.Kill();
+        }
+        uiImage.DOFillAmount(1 - (cur/max), duration).SetEase(ease);
+    }
+}
